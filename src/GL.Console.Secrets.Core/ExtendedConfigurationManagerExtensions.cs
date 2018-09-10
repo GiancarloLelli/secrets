fbb2cd4cf27f3479dc25b9dde34efc30297f694a
Giancarlo Lelli <gcarlo.lelli@live.com>
2018-09-10 15:51:10 +0200

    Added method for secret endpoint and minor refactoring

--- src/GL.Console.Secrets.Core/ExtendedConfigurationManagerExtensions.cs
+++ src/GL.Console.Secrets.Core/ExtendedConfigurationManagerExtensions.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿using GL.Console.Secrets.Core.Internal;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
-using System.Runtime.Serialization.Json;
-using System.Text;
 
 namespace GL.Console.Secrets.Extensions
 {
@@ -11,45 +10,45 @@ namespace GL.Console.Secrets.Extensions
     {
         public static void AddSecretsFromJson(this ExtendedConfigurationManager manager, string secretsPath)
         {
-            if (!File.Exists(secretsPath)) throw new FileNotFoundException("Unable to find secrets file", secretsPath);
+            if (!File.Exists(secretsPath))
+                throw new FileNotFoundException("Unable to find secrets file", secretsPath);
 
             using (var streamReader = new StreamReader(secretsPath))
             {
-                AddSecretsImpl(manager, streamReader.ReadToEnd());
+                InternalHelper.AddRange(manager, streamReader.ReadToEnd());
             }
         }
 
         public static void AddSecretsFromEndpoint(this ExtendedConfigurationManager manager, string endpointUrl)
         {
             if (!Uri.TryCreate(endpointUrl, UriKind.Absolute, out Uri uriResult) || (uriResult?.Scheme != Uri.UriSchemeHttp && uriResult?.Scheme != Uri.UriSchemeHttps))
-                throw new UriFormatException("Invalid endpoint URL provided");
+                throw new UriFormatException("Invalid endpoint URL provided or non HTTPS URL");
 
             using (var client = new HttpClient())
             {
                 var httpJsonResponse = client.GetStringAsync(new Uri(endpointUrl)).GetAwaiter().GetResult();
-                AddSecretsImpl(manager, httpJsonResponse);
+                InternalHelper.AddRange(manager, httpJsonResponse);
             }
         }
 
-        private static void AddSecretsImpl(ExtendedConfigurationManager manager, string json)
+        public static void AddSecretFromProtectedEndpoint(this ExtendedConfigurationManager manager, string endpointUrl, Func<string> tokenAction, Func<HttpResponseMessage, Dictionary<string, string>> deserializationHandler)
         {
-            if (string.IsNullOrEmpty(json)) throw new ArgumentException("Source JSON in empty");
+            if (tokenAction == null)
+                throw new ArgumentNullException(nameof(tokenAction));
 
-            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+            if (deserializationHandler == null)
+                throw new ArgumentNullException(nameof(deserializationHandler));
+
+            if (!Uri.TryCreate(endpointUrl, UriKind.Absolute, out Uri uriResult) || (uriResult?.Scheme != Uri.UriSchemeHttp && uriResult?.Scheme != Uri.UriSchemeHttps))
+                throw new UriFormatException("Invalid endpoint URL provided or non HTTPS URL");
+
+            var header = tokenAction.Invoke();
+            using (var client = new HttpClient())
             {
-                var settings = new DataContractJsonSerializerSettings { UseSimpleDictionaryFormat = true };
-                var serializer = new DataContractJsonSerializer(typeof(Dictionary<string, string>), settings);
-                var secrets = serializer.ReadObject(ms) as Dictionary<string, string>;
-                if (secrets != null)
-                {
-                    foreach (var secretItem in secrets)
-                    {
-                        if (manager.Secrets[secretItem.Key] == null)
-                        {
-                            manager.Secrets.Add(secretItem.Key, secretItem.Value);
-                        }
-                    }
-                }
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", header);
+                var httpResponse = client.GetAsync(new Uri(endpointUrl)).GetAwaiter().GetResult();
+                var settings = deserializationHandler.Invoke(httpResponse);
+                InternalHelper.AddRange(manager, settings);
             }
         }
     }
---
+++ src/GL.Console.Secrets.Core/Internal/InternalHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace GL.Console.Secrets.Core.Internal
+{
+    internal static class InternalHelper
+    {
+        public static void AddRange(ExtendedConfigurationManager manager, string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                throw new ArgumentException("Source JSON in empty");
+
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+            {
+                var settings = new DataContractJsonSerializerSettings { UseSimpleDictionaryFormat = true };
+                var serializer = new DataContractJsonSerializer(typeof(Dictionary<string, string>), settings);
+                var secrets = serializer.ReadObject(ms) as Dictionary<string, string>;
+                if (secrets != null)
+                {
+                    foreach (var secretItem in secrets)
+                    {
+                        if (manager.Secrets[secretItem.Key] == null)
+                        {
+                            manager.Secrets.Add(secretItem.Key, secretItem.Value);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void AddRange(ExtendedConfigurationManager manager, Dictionary<string, string> secrets)
+        {
+            if (secrets == null)
+                throw new ArgumentNullException(nameof(secrets));
+
+            foreach (var secretItem in secrets)
+            {
+                manager.Secrets.Add(secretItem.Key, secretItem.Value);
+            }
+        }
+    }
+}
